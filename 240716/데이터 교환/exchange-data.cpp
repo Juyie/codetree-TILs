8a32a8bda1a8becfd44e178b3a04f346a3708038
Juyie Hwang <71169201+Juyie@users.noreply.github.com>
2024-07-16 23:53:34 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/데이터 교환/exchange-data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, tempa = 5;
+    int b, tempb = 6;
+    int c, tempc = 7;
+    b = tempa;
+    c = tempb;
+    a = tempc;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
