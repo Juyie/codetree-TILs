2617177040a208c980eca7f0139e111ae75aa844
Juyie Hwang <71169201+Juyie@users.noreply.github.com>
2024-07-17 01:26:12 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    cin >> n;
+    string stars = "*";
+
+    for(int i = 0; i < n; i++){
+        cout << stars << endl;
+        stars += "**";
+    }
+
+    return 0;
+}
