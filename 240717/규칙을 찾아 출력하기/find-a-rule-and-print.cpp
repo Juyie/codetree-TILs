e306125e22f28603b6846d6d7c0824168898e1ab
Juyie Hwang <71169201+Juyie@users.noreply.github.com>
2024-07-17 17:25:16 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240717/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -7,10 +7,25 @@ int main() {
     cin >> n;
     
     for(int i = 0; i < n; i++){
+        cout << "* ";
+    }
+    cout << endl;
+
+    for(int i = 1; i < n-1; i++){
         for(int j = 0; j < n; j++){
-            cout << "* ";
+            if(j < i)
+                cout << "* ";
+            else if(j == n-1)
+                cout << "* ";
+            else
+                cout << "  ";
         }
         cout << endl;
     }
+
+    for(int i = 0; i < n; i++){
+        cout << "* ";
+    }
+
     return 0;
 }
