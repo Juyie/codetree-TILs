9daa5688ad352c047b93a6c8a217de4d76c1ff24
Juyie Hwang <71169201+Juyie@users.noreply.github.com>
2024-07-22 19:42:08 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 240722/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -25,6 +25,7 @@ int main() {
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             if(arr1[i][j] != arr2[i][j]) arr3[i][j] = 1;
+            else arr3[i][j] = 0;
         }
     }
 
